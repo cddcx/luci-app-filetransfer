e8eeb40291d3b2b6210feaaacaa3853703f7bc5d
cddcx <84568016+cddcx@users.noreply.github.com>
2023-12-06 21:54:26 +0800

    Update filetransfer.lua

--- luasrc/model/cbi/filetransfer.lua
+++ luasrc/model/cbi/filetransfer.lua
@@ -53,3 +53,112 @@ function Download()
 	fd:close()
 	http.close()
 end
+
+local dir, fd
+dir = "/tmp/upload/"
+nixio.fs.mkdir(dir)
+http.setfilehandler(
+	function(meta, chunk, eof)
+		if not fd then
+			if not meta then return end
+
+			if	meta and chunk then fd = nixio.open(dir .. meta.file, "w") end
+
+			if not fd then
+				um.value = translate("Create upload file error.")
+				return
+			end
+		end
+		if chunk and fd then
+			fd:write(chunk)
+		end
+		if eof and fd then
+			fd:close()
+			fd = nil
+			um.value = translate("File saved to") .. ' "/tmp/upload/' .. meta.file .. '"'
+		end
+	end
+)
+
+if luci.http.formvalue("upload") then
+
+
+	local f = luci.http.formvalue("ulfile")
+	if #f <= 0 then
+		um.value = translate("No specify upload file.")
+	end
+elseif luci.http.formvalue("download") then
+	Download()
+end
+
+local function getSizeStr(size)
+	local i = 0
+	local byteUnits = {' kB', ' MB', ' GB', ' TB'}
+	repeat
+		size = size / 1024
+		i = i + 1
+	until(size <= 1024)
+    return string.format("%.1f", size) .. byteUnits[i]
+end
+
+local inits, attr = {}
+for i, f in ipairs(fs.glob("/tmp/upload/*")) do
+	attr = fs.stat(f)
+	if attr then
+		inits[i] = {}
+		inits[i].name = fs.basename(f)
+		inits[i].mtime = os.date("%Y-%m-%d %H:%M:%S", attr.mtime)
+		inits[i].modestr = attr.modestr
+		inits[i].size = getSizeStr(attr.size)
+		inits[i].remove = 0
+		inits[i].install = false
+	end
+end
+
+form = SimpleForm("filelist", translate("Upload file list"), nil)
+form.reset = false
+form.submit = false
+
+tb = form:section(Table, inits)
+nm = tb:option(DummyValue, "name", translate("File name"))
+mt = tb:option(DummyValue, "mtime", translate("Modify time"))
+ms = tb:option(DummyValue, "modestr", translate("Attributes"))
+sz = tb:option(DummyValue, "size", translate("Size"))
+
+btnrm = tb:option(Button, "remove", translate("Remove"))
+btnrm.render = function(self, section, scope)
+	self.inputstyle = "remove"
+	Button.render(self, section, scope)
+end
+
+btnrm.write = function(self, section)
+	local v = luci.fs.unlink("/tmp/upload/" .. luci.fs.basename(inits[section].name))
+	if v then table.remove(inits, section) end
+	return v
+end
+
+function IsIpkFile(name)
+	name = name or ""
+	local ext = string.lower(string.sub(name, -4, -1))
+	return ext == ".ipk"
+end
+
+btnis = tb:option(Button, "install", translate("Install"))
+btnis.template = "filetransfer/other_button"
+btnis.render = function(self, section, scope)
+	if not inits[section] then return false end
+	if IsIpkFile(inits[section].name) then
+		scope.display = ""
+	else
+		scope.display = "none"
+	end
+	self.inputstyle = "apply"
+	Button.render(self, section, scope)
+end
+
+btnis.write = function(self, section)
+	local r = luci.sys.exec(string.format('opkg --force-depends install "/tmp/upload/%s"', inits[section].name))
+	form.description = string.format('<span style="color: red">%s</span>', r)
+end
+
+return ful, fdl, form
